dde85ee2d534c6b5e91231363be4b75d9ef8e4a8
TerrenceJosh <terrencejosh05@gmail.com>
2026-01-28 22:32:54 +0800

    change it

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="container">
     <header> <h1>Personal Profile Web page </h1> </header>
-    
-    
-    <style src="./css/personalprofile.css"></style>
+
+<style src="./css/personalprofile.css"></style>
