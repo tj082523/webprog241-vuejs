4b2a4db8314bf55663c80ce045c40057726ba9af
TerrenceJosh <terrencejosh05@gmail.com>
2026-02-05 21:58:15 +0800

    added personalcss

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,6 +1,22 @@
 <template>
-<div class="container">
-    <header> <h1>Personal Profile Web page </h1> </header>
-    
-    <style src="./css/personalprofile.css"></style>
+  <div class="container">
+    <header>
+      <h1>Personal Profile Web Page</h1>
+    </header>
+
+    <section class="profile">
+      <p><strong>Name:</strong> Terrence Corpuz</p>
+      <p><strong>Course:</strong> BSIT</p>
+      <p><strong>School:</strong> Asia Pacific College</p>
+      <p><strong>Bio:</strong> Loves to Ride a Motorcycle 🚀</p>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "PersonalProfile"
+}
+</script>
+
+<style src="./css/personalprofile.css"></style>
