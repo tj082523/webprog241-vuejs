16dddc7691e54430af7ba4edad4bf1d8c61b6965
TerrenceJosh <terrencejosh05@gmail.com>
2026-02-06 16:08:02 +0800

    fixed version

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -8,5 +8,5 @@ import PersonalProfile from './components/PersonalProfile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.mount('#app')
 app.component('personal-profile', PersonalProfile)
+app.mount('#app')
