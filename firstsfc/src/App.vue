a780b1191a6e7e1a69a722ba30db1ffd790a64f7
TerrenceJosh <terrencejosh05@gmail.com>
2026-02-07 23:29:11 +0800

    Task 03 Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,17 +1,13 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-const instruments = ref([])
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
-onMounted(() => {
-   getInstruments()
-})
-</script>
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <h1>Food</h1>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
+
+<script></script>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import PersonalProfile from './components/PersonalProfile.vue'
+import Country from './components/Country.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
-app.component('personal-profile', PersonalProfile)
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
