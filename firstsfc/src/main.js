a736427ebec40279ce0ee69581f4b9c898d3afbd
TerrenceJosh <terrencejosh05@gmail.com>
2026-02-07 23:35:57 +0800

    change this mainjs

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import Country from './components/Country.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('country', Country)
+app.component('food-item2', FoodItem2)
+app.component('personal-profile', PersonalProfile)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.mount('#app')
